1090c878df7ebaaac99a8a73f512c2918530629e
neonwednesdays <dxiao.ns@gmail.com>
2015-02-03 01:23:45 -0500

    Can dynamically attach project items now :)

--- js/elementten.js
+++ js/elementten.js
@@ -2,6 +2,8 @@
 $(document).ready(function(){
 	//what tab did ya click?
 	$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+		window.scrollTo(0,0);
+
 		var tabname = $(e.target).attr('aria-controls');
 		$('.jumbo-info').empty();
 		if(tabname == 'home'){
@@ -22,8 +24,17 @@ function getText(tabname){
 		case 'home':
 			return 'Daniel Xiao is a multidisciplinary developer and designer who has a passion for creating interactive projects. He works with Javascript, JQuery, HTML, and CSS to design web applications. He works in Flash + Actionscript, Unity + C#, and Photoshop to design games. Look at his projects below!';
 			break;
+		case 'projects':
+			return 'major projects Daniel has made';
+			break;
+		case 'games':
+			return 'a collection of game projects';
+			break;
+		case 'designs':
+			return 'ideation from graphic design to functional design';
+			break;
 		case 'about':
-			return 'More about Daniel Xiao';
+			return 'more about Daniel Xiao';
 			break;
 		default:
 			return 'Oh, you found the default text.'
--- js/projectObjects.js
+++ js/projectObjects.js
@@ -1,6 +1,46 @@
-//creates a project and then does a lot of stuff
-function createProject(){
+var mainprojects = [];//project page
+var games = [];
+var designs = [];
+$(document).ready(function(){
+	initAllProjects();
+	createProjects(mainprojects, 'projects');
+	//clicking on a project item
+	$('.item-clickable').click(function(){
+		$('#myModal').modal('show');
+		$('.modal-title').html(mentorweb.name);
+		$('.modal-body').html(mentorweb.descriptionString);
+	})
+	//clicking the view project button
+	$('.view-project').click(function(){
+		window.location = mentorweb.weburl;
+	});
+	$('#myModal').on('hidden.bs.modal', function (e) {
+	  $('.modal-body').empty();
+	})
+});
 
+//creates a project and then does a lot of stuff
+function createProjects(list, type){
+	for(var i=0;i<list.length;i++){
+		var rownum = i===0 ? 0:Math.floor(i/3);
+
+		if(i%3 === 0){//make a new row for every third project
+			//make the card (row div)
+			$('<div></div>',{
+				class: type +'-page '+ rownum + ' row'
+			}).appendTo('#'+type);
+		}
+		//make the column div
+		$('<div></div>',{
+			class: 'item-clickable col-md-4 ' + type + '-page' + '-' + i
+		}).appendTo('.'+type+'-page'+'.'+rownum);
+
+		var contentString = '<img src="images/'+list[i].subname+'.png">';//add the image
+		contentString += '<h4 class="title">' + list[i].name+ '<span class="glyphicon glyphicon-chevron-right"></span></h4>'
+		$('.'+type+'-page'+'-'+i).append(contentString);
+
+		
+	}
 }
 //give the project a click listener that will change modal data
 function giveProjectOnClickListener(){
@@ -11,8 +51,27 @@ function projectObject(name, imgpath){
 	this.name = name;
 	this.imgpath = imgpath;
 	this.descriptionString = '';//give me a description!
-	//maybe a url var?
+	//url var?
 }
 //There's a lot of crap to copy paste so let's make it easier
-var mentorweb = new projectObject('Mentor Web', 'images/mentorweb.png');
-mentorweb.descriptionString = 'Web application made using AngularJS, html, css. Made to match mentors and mentees.'
+var mentorweb = new projectObject('MentorWeb', 'images/mentorweb.png');
+mentorweb.subname = 'mentorweb';
+mentorweb.weburl = 'http://dev.m.gatech.edu/d/mosborne8/w/MentorAngular/';
+mentorweb.descriptionString = 'Web application made using AngularJS, html, css. Made to match mentors and mentees.'+
+				'<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://www.youtube.com/embed/c6vKkqgn3K4" frameborder="0" allowfullscreen></iframe></div>'
+
+var betrayalhelper = new projectObject('Betrayal at House on the Hill Helper', 'images/betrayalhelper.png');
+betrayalhelper.subname = 'betrayalhelper';
+betrayalhelper.weburl = 'http://www.elementten.com/BetrayalHelper/';
+betrayalhelper.descriptionString = '';
+
+function initAllProjects(){
+	mainprojects = [];
+	mainprojects.push(mentorweb);
+	mainprojects.push(betrayalhelper);
+	mainprojects.push(mentorweb);
+	mainprojects.push(betrayalhelper);
+	mainprojects.push(mentorweb);
+	mainprojects.push(betrayalhelper);
+
+}
